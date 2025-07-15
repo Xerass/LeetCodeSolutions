a4f64866ab3051fac450bf9a5dc0a8cf89c23497
Xerass <144137270+Xerass@users.noreply.github.com>
2025-07-15 12:42:45 +0800

    Update solution.py

--- EasyCategory/TwoSum/solution.py
+++ EasyCategory/TwoSum/solution.py
@@ -1,1 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #approach with a complement approach
+        num_idx  = {}
+
+        for i, num in enumerate(nums):
+            #iterate over the nums, each num subtract with target
+            complement = target - num
+
+            #if the complement was already in the array then we can stop and return the indices
+            if complement in num_idx:
+                return [num_idx[complement], i]
+            
+            #if not found store the num as a key along with its index
+            num_idx[num] = i
 
