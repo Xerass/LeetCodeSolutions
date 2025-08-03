a3206a013a11bb490f2960241eb3ccbe073e73a3
Xerass <144137270+Xerass@users.noreply.github.com>
2025-08-03 10:23:52 +0800

    Create solution.py

---
+++ EasyCategory/InsertAt/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+        
+        #if not found then we insert to the left since it should be in between the last two digits searched
+        return left
