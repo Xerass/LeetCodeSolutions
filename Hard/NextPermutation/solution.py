a59272dc14c3e0dd1c50fae409cd812452db1a16
Xerass <144137270+Xerass@users.noreply.github.com>
2025-11-16 12:50:33 +0800

    Update solution.py

--- Hard/NextPermutation/solution.py
+++ Hard/NextPermutation/solution.py
@@ -26,10 +26,14 @@ class Solution(object):
             nums[i], nums[j] = nums[j], nums[i]
 
         left, right = i + 1, n - 1
-        nums[left], nums[right] = nums[right], nums[left]
-        left += 1
-        right -= 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
 
         return nums
 
+
+
+        
 
