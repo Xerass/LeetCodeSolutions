2d6bbbe84054617d4eaec84c0505a335e88add78
Xerass <144137270+Xerass@users.noreply.github.com>
2025-12-18 19:37:43 +0800

    Update solution.py

--- EasyCategory/MergeSortedLists/solution.py
+++ EasyCategory/MergeSortedLists/solution.py
@@ -1,34 +1,34 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
 
-        dummy = ListNode()
-        current = dummy
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyNode = ListNode()
+        curr = dummyNode
 
-        #traverse both lists and append the smaller one to current
+        #dummy node will point to head
+        #temp node will hold temporary nexts
+        #it's already sorted beforehand so we can just compare them node by node and attach them accordingly to each other
         while list1 and list2:
-            if list1.val < list2.val:
-                current.next = list1
-                list1 = list1.next
-            else:
-                current.next = list2
+            if list1.val > list2.val:
+                #current builds the actual linked list we will pass
+                curr.next = list2
+                #move pointer forward
                 list2 = list2.next
-            current = current.next
+            else:
+                curr.next = list1
+                list1 = list1.next
+            
+            #make node as the next so we can attach more nodes from there
+            curr = curr.next
 
-        #remaining stuff
-        if list1:
-            current.next = list1
-        else:
-            current.next = list2
-        
-        return dummy.next
+        if list1 or list2:
+            #cases when there are remaining elements in one of the lists
+            curr.next = list1 or list2
+
+        return dummyNode.next
+
         
