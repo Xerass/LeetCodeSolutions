25856786fca1d1dd9dff5cf3cd4c7ed2187b9795
Xerass <144137270+Xerass@users.noreply.github.com>
2025-12-26 10:11:47 +0800

    Update solution.py

--- EasyCategory/ValidParen/solution.py
+++ EasyCategory/ValidParen/solution.py
@@ -1,20 +1,30 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        match = {')': '(', ']': '[', '}': '{'}
-        openings = set(match.values())
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {   "{":"}",
+                    "(":")",
+                    "[":"]"}
+
         stack = []
 
-        for ch in s:
-            if ch in openings:
-                stack.append(ch)
-            elif ch in match:
-                if not stack or stack[-1] != match[ch]:
-                    return False
-                stack.pop()
-        
-        return not stack
+        #create a set of the closers as we will use pair dict to look up for keys (openers)
+        closers = set(pairs.values())
 
+        for char in s:
+            if  char in pairs:
+                #append is the "push"
+                stack.append(char)
+            if char in closers:
+                #is a closer so we need to check if a pop matches it, else false
+                #stack empty but char was still read, must mean excess
+                if not stack:
+                    return False
+        
+                #pop a char from the stack, if it is not within the pairs invalid
+                evalChar = stack.pop()
+
+                #if the opener (the ones we push to stack) does not match the closer, false
+                if pairs[evalChar] != char:
+                    return False
+        
+        #if it passes the loop, must mean it is valid (check if empty first tho)
+        return not stack
