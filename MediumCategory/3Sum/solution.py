53a8569d9bf745705e8095f6933fbc797fe65f08
Xerass <144137270+Xerass@users.noreply.github.com>
2026-01-19 09:34:04 +0800

    Update solution.py

--- MediumCategory/3Sum/solution.py
+++ MediumCategory/3Sum/solution.py
@@ -1,43 +1,34 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #3 sum is just an extra step 2 sum
+        res = []
         nums.sort()
-        result = []
-        
 
-        for i in range(len(nums) - 2):
-            #skip dupes
+        for i in range(len(nums)):
+            if nums[i] > 0: 
+                break
             if i > 0 and nums[i] == nums[i-1]:
+                #duplicate skip
                 continue
-
-            #set up 2 pointers
-            left, right = i +1 , len(nums) - 1
-
+            
+            #this serves as an "anchor" after this we just do a normal 2 sum
+            left = i + 1
+            right = len(nums) - 1
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-
-                    #skip dupes for either nums
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
-                    #move pointers
+                currSum =  nums[i] + nums[left] + nums[right]
+                if currSum == 0:
+                    res.append([nums[i],nums[left],nums[right]])
                     left += 1
                     right -= 1
 
-                elif total <  0:
+                    #skip left dupes
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                elif currSum < 0:
                     left += 1
                 else:
                     right -= 1
-        return result
-                        
 
+        return res
         
