0edf3f7019c56cbb133a6279db155f86f53b73b0
Xerass <144137270+Xerass@users.noreply.github.com>
2026-05-09 17:22:27 +0800

    Create sol.sql

---
+++ PandasAndSQL/Easy/SalesAnalysisIII/sol.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT p.product_id, p.product_name
+FROM Product as p
+JOIN Sales as s ON s.product_id = p.product_id
+GROUP BY p.product_id, p.product_name
+#we cant use between since all it cares about is IF it was sold in that range
+#this check FORCES it to be in that range only
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31';
